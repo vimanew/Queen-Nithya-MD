d4bff6d57989c861ed55830e0f89672864155687
Dileepa_Tech <103705712+DileepaTech@users.noreply.github.com>
2024-03-15 11:54:41 +0530

    ❤️Queen Nithya MD❤️

--- commands/_plugins.js
+++ commands/_plugins.js
@@ -1,24 +1,3 @@
-/**
-========================================================
-      █▄▀ █ █▄ █ ▄▀     █▄ ▄█ █▀▄
-      █ █ █ █ ▀█ ▀▄█ ▀▀ █ ▀ █ █▄▀
-========================================================
- Copyright (C) 2022.                                                                                        
- Licensed under the  GPL-3.0 License;                                                      
- You may not use this file except in compliance with the License.    
- It is supplied in the hope that it may be useful                                     
- * @project_name : KING-MD                                                                   
- * @author : naveeddogar <https://github.com/naveeddogar>   
- * @description : KING-MD ,A Multi-functional whatsapp bot.       
- * @version 1.0.1                                                                                             
- ========================================================
- **/
- 
- 
-const axios = require('axios');
-const fs = require('fs-extra')
-const { plugins,plugindb, remove, isUrl,cmd , tlang  , Config } = require('../lib')
-//---------------------------------------------------------------------------
 cmd({
         pattern: "plugins",
         alias :['plugin'],
