3c7b2f878066ab409229b7b4a9a366191e3f0c12
Dileepa_Tech <103705712+DileepaTech@users.noreply.github.com>
2024-03-15 12:02:08 +0530

    ❤️Queen Nithya MD❤️

--- commands/fun.js
+++ commands/fun.js
@@ -1,18 +1,4 @@
-/**
-========================================================
-      █▄▀ █ █▄ █ ▄▀     █▄ ▄█ █▀▄
-      █ █ █ █ ▀█ ▀▄█ ▀▀ █ ▀ █ █▄▀
-========================================================
- Copyright (C) 2022.                                                                                        
- Licensed under the  GPL-3.0 License;                                                      
- You may not use this file except in compliance with the License.    
- It is supplied in the hope that it may be useful                                     
- * @project_name : KING-MD                                                                   
- * @author : naveeddogar <https://github.com/naveeddogar>   
- * @description : KING-MD ,A Multi-functional whatsapp bot.       
- * @version 1.0.1                                                                                             
- ========================================================
- **/
+
 
 const { dare, truth, random_question } = require('../lib/truth-dare.js')
 const axios = require('axios')
